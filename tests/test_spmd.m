63355c346015a9d9b3ffccf14a20b90944866281
Guillaume Flandin <guillaume.flandin@gmail.com>
2021-11-16 22:36:59 +0000

    Compute diagnostic statistics in CI

--- tests/test_spmd.m
+++ tests/test_spmd.m
@@ -1,3 +1,11 @@
 function test_spmd
 
-getenv('GLM_PATH')
+GLM_PATH = getenv('GLM_PATH');
+
+spm defaults fmri
+spm_jobman initcfg
+
+matlabbatch{1}.spm.tools.spmd.compute.spmmat = {fullfile(GLM_PATH,'SPM.mat')};
+matlabbatch{1}.spm.tools.spmd.compute.outdir = {''};
+
+spm_jobman('run',matlabbatch);
